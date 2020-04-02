cef0ac60753e22f574d1650a2c193d6a83e57fc8
2lipe <2lipe>
2020-04-02 02:14:58 -0300

    challenge29 - concluido

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,6 +1,5 @@
-(function() {
+(function( DOM ) {
   'use strict';
-
   /*
   Vamos estruturar um pequeno app utilizando módulos.
   Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
@@ -36,4 +35,80 @@
   que será nomeado de "app".
   */
 
-})();
+  const app = ( function appController() {
+    return {
+
+      init() {
+        console.log( 'app init' );
+        this.companyInfo();
+        this.initEvents();
+      },
+
+      initEvents() {
+        const $form = new DOM( '[data-js="form-register"]' );
+        $form.on( 'submit', this.handleSubmit, false );
+      },
+
+      handleSubmit( e ) {
+        e.preventDefault();
+        const $tableCar = new DOM( '[data-js="table-car"]' );
+        $tableCar.get().appendChild( app.addNewCar() );
+      },
+
+      addNewCar() {
+        let $fragment = document.createDocumentFragment();
+        let $tr = document.createElement( 'tr' );
+        let $tdImage = document.createElement( 'td' );
+        let $image = document.createElement( 'img' )
+        let $tdBrand = document.createElement( 'td' );
+        let $tdYear = document.createElement( 'td' );
+        let $tdPlate = document.createElement( 'td' );
+        let $tdColor = document.createElement( 'td' );
+
+        $image.setAttribute( 'src', new DOM( '[data-js="car-image"]' ).get().value );
+        $tdImage.appendChild( $image );
+
+        $tdBrand.textContent = new DOM( '[data-js="car-brand"]' ).get().value;
+        $tdYear.textContent = new DOM( '[data-js="car-year"]' ).get().value;
+        $tdPlate.textContent = new DOM( '[data-js="car-plate"]' ).get().value;
+        $tdColor.textContent = new DOM( '[data-js="car-color"]' ).get().value;
+
+        $tr.appendChild( $tdImage );
+        $tr.appendChild( $tdBrand );
+        $tr.appendChild( $tdYear );
+        $tr.appendChild( $tdPlate );
+        $tr.appendChild( $tdColor );
+
+        return $fragment.appendChild( $tr );
+      },
+
+      companyInfo() {
+        console.log( 'Company Info is OK' )
+        const companyPath = './company.json';
+        const ajax = new XMLHttpRequest();
+
+        ajax.open( 'GET', companyPath, true );
+        ajax.send();
+        ajax.addEventListener( 'readystatechange', this.getCompanyInfo, false );
+      },
+
+      getCompanyInfo() {
+        console.log( 'Get Info is OK' )
+        if( !app.isReady.call( this ) )
+          return;
+
+        const data = JSON.parse( this.responseText );
+        const $companyName = new DOM( '[data-js="company-name"]' );
+        const $companyPhone = new DOM( '[data-js="company-phone"]' );
+        $companyName.get().textContent = data.name;
+        $companyPhone.get().textContent = data.phone;
+      },
+
+      isReady() {
+        return this.readyState === 4 && this.status === 200;
+      }
+    };
+  })();
+  app.init();
+
+})( window.DOM );
